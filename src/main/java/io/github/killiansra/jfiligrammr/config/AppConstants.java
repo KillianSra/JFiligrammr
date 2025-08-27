9bdbb6d81357c3625eb6d6a52bd961f822597914
Killian SIERRA <killian.sierra@yahoo.fr>
2025-08-27 19:43:22 +0200

    Replace use of home directory with dedicated OS-specific config directories

--- src/main/java/io/github/killiansra/jfiligrammr/config/AppConstants.java
+++ src/main/java/io/github/killiansra/jfiligrammr/config/AppConstants.java
@@ -1,9 +1,11 @@
 package io.github.killiansra.jfiligrammr.config;
 
+import io.github.killiansra.jfiligrammr.util.FileUtil;
+
 public class AppConstants
 {
     public static final String VALID_EXTENSION = "pdf";
-    public static final String UPLOAD_DIR_NAME = System.getProperty("user.home") + "/JFiligrammr";
+    public static final String UPLOAD_DIR_NAME = FileUtil.getUserConfigPath();
     public static final String RESOURCE_BASE_PATH = "/io/github/killiansra/jfiligrammr/";
     public static final float DPI = 150.0F;
     public static final int SCALE = (int) DPI / 72;
